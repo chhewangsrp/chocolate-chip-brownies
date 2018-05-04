6d0da79679414cd8d0954c5043f3593643e19bb5
Chhewang Sherpa <chhewangsrp@gmail.com>
2018-05-04 09:29:34 -0400

    Data fetched using axios from NYC data

--- final_project/src/components/NavBar.js
+++ final_project/src/components/NavBar.js
@@ -1,8 +1,8 @@
 import React from "react";
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import logo from "./Logo1.png"
 import './index.css'
-import api from "./api"
 
 
 
@@ -16,25 +16,33 @@ class NavBar extends React.Component
 
 	}	
 
-	componentWillMount() {
-		api.getNeighborhood().then((res) => {
-			this.setState({
-				Neighborhood: res.Neighborhood 
-			})
-		});
+	componentDidMount() {
+		axios.get(`https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD`)
+      .then(res => {
+        const Neighborhoods = res.data.data;
+
+        this.setState({ Neighborhood: Neighborhoods });
+
+        console.log("this is data ", res);
+      }).catch(err=>{
+      		console.log("Error Occured Fetching: ",err);
+      });
 	}
 
 
 
 	render() {
-		console.log(this.state);
+		const dropDownMenu = this.state.Neighborhood.map((neighborhood, index)=> 
+		<option>	
+			{neighborhood[10]} 
+ 		 </option> )
+		
     return (
         <div className="container">
 
-        	
-            <nav className="navbar navbar-expand-lg ">	
+        	 <nav className="navbar navbar-expand-lg ">	
                 <ul className="navbar-nav">
-                	
+
                 	<li className= "nav-item">
                 		<img className= "logo" src={logo}/> 
                 	</li>
@@ -44,13 +52,19 @@ class NavBar extends React.Component
 
                     <li className="nav-item">
                         <a className="nav-link" href="#">How it works</a>
-                    </li> 
-                    
+                    </li>
+               <li>      
+                <div className="dropdown">
+                	<select className="cityName">
+                		<option className="Selected"> Choose a city </option>
+                		{dropDownMenu}
+                	</select>
+   				</div>
+				</li>	
                     
                 </ul>
             </nav>
-            <div className="content">
-            </div>
+         
         </div>
     );
 }
